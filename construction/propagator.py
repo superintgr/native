dd2a9dec78f1ddec6ae8924342dbf99dda405a6f
superintgr <155528797+superintgr@users.noreply.github.com>
2024-04-18 10:36:56 -0400

    Update propagator.py

--- construction/propagator.py
+++ construction/propagator.py
@@ -1,1 +1,63 @@
-# deleted and moved
+import torch
+
+class Native(nn.Module):
+    def construct(self, output):
+        input = self.solve(self, output)
+        state = self(input).T @ output
+        return state
+
+    def __enter__(self, mode):
+        for file in self.files:
+            window = open(file, mode)
+            interpretation = self(window)
+            structure = self.construct(interpretation)
+            self.apply_structure(structure)
+
+    def __exit__(self, closure, callback):
+        # If the closure does not satisfy file operation guide, use the callback to reinterpret until steady state is achieved.
+        pass
+
+    def __call__(self, input):
+        output = self.processor(input)
+        return output
+
+class Super(nn.Module):
+    def transform(self, state):
+        clone = Native(self.last_memory)
+        task = clone.construct(state)
+        other = Native(task)
+        output = cloned(other.construct(task)) @ state
+        return output
+
+    def squeeze(self, data):
+        target = torch.zeros_like(data)
+        angles = self.find_relative_phases(match=target, channels=2)
+        left, right = self.apply_shift(*angles)
+        mid, side = self.encode_outer(left, right)
+        memory = mid.T @ data
+        padding = memory @ side.T
+        mapping = self.encode_inner(memory, padding)
+        scalars = self.solve_linear(mapping, target)
+        vectors = self.decode_inner(*self.decode_outer(left * scalars[0], right * scalars[1]))
+        return vectors
+
+    def unsqueeze(self, information):
+        pass
+
+
+def find_solution(left, center, right):
+    """
+    Compute the tuple (X, Y) such that ((left * X) + (right * Y)) / 2 = center
+    """
+    X = 2 * center - right
+    Y = 2 * center - left
+    return (X, Y)
+
+def find_question(side, mid):
+    """
+    Compute the tuple (A, B) such that mid + side = A and mid - side = B.
+    """
+    A = mid + side
+    B = mid - side
+    return (A, B)
+
