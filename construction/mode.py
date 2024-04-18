fdfaf101a1b13f0538f63447e310fc1a04b6c16b
superintgr <155528797+superintgr@users.noreply.github.com>
2024-04-18 19:12:03 -0400

    Create mode.py

---
+++ construction/mode.py
@@ -0,0 +1,11 @@
+"""part:1
+# ensamble of personalities in a room
+# every personality participate in the translation of room modes
+# each personality contributes eigenvalue
+# how do they get activated?
+"""
+
+"""part:2
+# the ensamble environment runs in a virtual machine
+# encoding-embedding-processing-decoding will take place in container
+"""
