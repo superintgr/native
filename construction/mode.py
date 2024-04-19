e1585e2068ac438b44534b0eb5a1212185ea59c4
superintgr <155528797+superintgr@users.noreply.github.com>
2024-04-19 01:01:15 -0400

    Update mode.py

--- construction/mode.py
+++ construction/mode.py
@@ -8,4 +8,28 @@
 """part:2
 # the ensamble environment runs in a virtual machine
 # encoding-embedding-processing-decoding will take place in container
+# docker container will be configured
 """
+
+"""part:3
+# finetune some language model of tasks
+# add the pretrained model on top and add additional task specific layer at the bottom
+"""
+
+
+
+possible = [
+    'reading a file from cpu memory as byte valued object',
+    'sample part or whole of the set of bytes at file path',
+    'copy part or whole of the byte contents to other compatible memory addresses',
+    'create a custom dictionary with choosen named objects as keys and arbitrary other objects as values' 
+    'assigning a name to each byte elements and storing them as record element in a dictionary',
+    'assigning a name to each byte sequence and storing them as record element in a dictionary',
+    'assigning a name to each record element in a dictionary and storing the relation into named variable',
+    'create a random association with matrix to some named variable content',
+    'performing linear algebra operations on matrix and named content where the variable represents vector entity',
+    'storing the matrix-vector product of all such named contents as records in a dictionary',
+    'reference to readout of the byte elements for legitimate computation performed over the data at that path',
+    'some matrix could be constructed whose product with the named vector represented information corresponds to the linear combination of some unknown other legitimate vector',
+    'matrix-matrix product between the ideal constructor of the information variables and approximated random matrix correlates would produce a matrix with correlation intact',
+]
