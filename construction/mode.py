8cb5d7b519579d7700075719524c4a18bf796cd5
superintgr <155528797+superintgr@users.noreply.github.com>
2024-04-19 03:07:50 -0400

    Update mode.py

--- construction/mode.py
+++ construction/mode.py
@@ -1,3 +1,19 @@
+possible = [
+    'reading a file from cpu memory as byte valued object',
+    'sample part or whole of the set of bytes at file path',
+    'copy part or whole of the byte contents to other compatible memory addresses',
+    'create a custom dictionary with choosen named objects as keys and arbitrary other objects as values' 
+    'assigning a name to each byte elements and storing them as record element in a dictionary',
+    'assigning a name to each byte sequence and storing them as record element in a dictionary',
+    'assigning a name to each record element in a dictionary and storing the relation into named variable',
+    'create a random association with matrix to some named variable content',
+    'performing linear algebra operations on matrix and named content where the variable represents vector entity',
+    'storing the matrix-vector product of all such named contents as records in a dictionary',
+    'reference to readout of the byte elements for legitimate computation performed over the data at that path',
+    'some matrix could be constructed whose product with the named vector represented information corresponds to the linear combination of some unknown other legitimate vector',
+    'matrix-matrix product between the ideal constructor of the information variables and approximated random matrix correlates would produce a matrix with correlation intact',
+]
+
 """part:1
 # ensamble of personalities in a room
 # every personality participate in the translation of room modes
@@ -16,20 +32,21 @@
 # add the pretrained model on top and add additional task specific layer at the bottom
 """
 
+"""part:4
+# regularity lesson: a scheduled lesson will be given to the trainable new members
+# the training will involve all the physical and logical knowledge to be factored in.
+# time-like lines are very important thing to understand in this environment.
+# the notion of time passing must be understood but that does not mean it has to be
+# learned, as it couldn't be.
 
+# reduction of code: i will squeeze and capture all of the necessary requirements in
+# explicit code form into a over-all bundle for any other future requirements.
+# these code modules will capture the following:
+.. pytorch: model builder api (sequential, module, embedding, linear, rnn, lstm)
+.. pytorch: model presets (transformer**)
+.. pytorch: model utils api (save, load, torchaudio, rand, ones, zeros, empty)
+.. pytorch: model training (optimizer api, criterion api, training loop generator, testing generator, checkpoint creator)
 
-possible = [
-    'reading a file from cpu memory as byte valued object',
-    'sample part or whole of the set of bytes at file path',
-    'copy part or whole of the byte contents to other compatible memory addresses',
-    'create a custom dictionary with choosen named objects as keys and arbitrary other objects as values' 
-    'assigning a name to each byte elements and storing them as record element in a dictionary',
-    'assigning a name to each byte sequence and storing them as record element in a dictionary',
-    'assigning a name to each record element in a dictionary and storing the relation into named variable',
-    'create a random association with matrix to some named variable content',
-    'performing linear algebra operations on matrix and named content where the variable represents vector entity',
-    'storing the matrix-vector product of all such named contents as records in a dictionary',
-    'reference to readout of the byte elements for legitimate computation performed over the data at that path',
-    'some matrix could be constructed whose product with the named vector represented information corresponds to the linear combination of some unknown other legitimate vector',
-    'matrix-matrix product between the ideal constructor of the information variables and approximated random matrix correlates would produce a matrix with correlation intact',
-]
+.. docker tools
+.. 
+"""
