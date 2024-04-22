d069568609a232f39bf3827997be7531152eb520
superintgr <155528797+superintgr@users.noreply.github.com>
2024-04-22 18:36:30 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import json
 
-model = torch.nn.Transformer(
+# model = torch.nn.Transformer(
     d_model=512,
     nhead=8,
     num_encoder_layers=6,
@@ -18,8 +18,26 @@ model = torch.nn.Transformer(
     device=None,
     dtype=None)
     
-with open("data.json") as f:
-    data = json.load(f)
+"""
+These models would not allow sufficient learning to occur, as their primary focus is on the scalability of the system.
 
-input = torch.nn.functional.one_hot(data["input"], num_classes=512)
-output = torch.nn.functional.one_hot(data["output"], num_classes=512)
+Here are some problems:
+-- does not allow any type other than float or complex
+-- cannot process integer or long type
+-- very large
+"""
+
+"""Proposed architecture:
+
+There will be an environment and everything will be modeled after that.
+
+**Environment**
+
+Enables the following features:
+[definiteness] every object placed inside the substrate have definite positional properties
+[dual] objects have their internal and external attributes
+[conservation] an object with a position inside the substrate conserves some special quantity
+[modeling] dynamics within the substrate are described geometrically
+[dimensionality] environment constructs a three-dimensional structure
+
+Any programmable structure can interact with the environment and their unique properties will be held at the local substrate, where their intrinsic properties are entirely separate from their embedded information.
