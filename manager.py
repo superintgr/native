16a23fb63173d1738a4989ee8edf14a331bbe14a
superintgr <155528797+superintgr@users.noreply.github.com>
2024-04-30 07:50:51 -0400

    Update manager.py

--- manager.py
+++ manager.py
@@ -11,3 +11,32 @@ from inductor import create_agent
 --- it can mark trajectories in terms of optimal or organizational
 -- 
 """
+
+
+
+resource = sample taken from empty room (R, D)
+
+catalog the resource attribute that are usable for transformation tasks
+a transformation task would be in terms of a state of static object with known properties whose attributes are reshaped by the constructor thus turning the object attributes into another variable attributes whose properties satisfy the task description.
+
+
+generic attributes are used for the properties of those values along with a matrix into a system of equations.
+
+this system of linear equations contains the variable whose properties are determined by the convergent into the target state from their generic counterpart.
+
+this constitutes a substrate with its own resource matrix which could be used for arbitrary functions up to the level that is physically possible.
+
+
+creating a reversible computation task for transforming some generic source into a usable (possible) substrate attributes.
+
+this task is a permutation over the set of at least two parameters defining the substrate attribute.
+
+computing function (C[pi]) over [S] is the union of all x -> pi(x) where x is a possible valid attribute in S.
+
+def compute(permutator, source):
+    for x in source:
+        y = permutator(x)
+        yield union(x, y)
+
+
+
