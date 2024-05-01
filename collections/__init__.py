f1fbfda2fd814e1c68246192845959d9a57ac00c
superintgr <155528797+superintgr@users.noreply.github.com>
2024-05-01 16:08:47 -0400

    Update __init__.py

--- collections/__init__.py
+++ collections/__init__.py
@@ -1,1 +1,20 @@
 # Collection of points, points of points, associations of pairs
+
+
+one = lambda x, y: x is y and y is x
+two = lambda x, y, z: x is y and y is z or z is x
+three = lambda a, b: a == b and isinstance(b, type(a))
+
+
+successor = lambda number, other: successor(number) is successor(other)
+natural = lambda number: successor(number) is successor(next(number))
+real = lambda number: successor(number) is not natural(number)
+
+
+
+
+#### THE ABOVE CODE ESTABLISHES SOME BASE TRIPLETS WHICH ARE NOT AXIOMS
+
+
+
+
