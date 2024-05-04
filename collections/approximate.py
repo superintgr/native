bd7b53a04e6252ab87d3900f81cd0c489eafe888
superintgr <155528797+superintgr@users.noreply.github.com>
2024-05-03 23:21:47 -0400

    Create approximate.py

---
+++ collections/approximate.py
@@ -0,0 +1,13 @@
+import networkx as nx
+
+master = nx.Graph()
+
+"""Policy
+(1) `master` is an undirected graph object which should be seen as network substrate.
+(2) new information is processed by the master node where it compiles the data into 2 dimensional object code.
+(3) for information to be new there has to be distinguishable features of the data that constitute extending the population size accordingly.
+(4) if information can be present in a state of an object, then that object must have at least two distinguishable states where their attributes can encode information.
+(5) if information can be encoded in a pair of states of two objects that each have necessary properties for storing the data, then the two can carry out processing of information too.
+(6) if two objects that can process information have unity, then the union of the two states cannot hold information.
+"""
+
