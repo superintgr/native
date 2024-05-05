1a3162947e1cd22bce070add238918fcc1a5e928
superintgr <155528797+superintgr@users.noreply.github.com>
2024-05-05 10:27:51 -0400

    Create anamoly.py

---
+++ collections/anamoly.py
@@ -0,0 +1,27 @@
+import None
+
+image = """### Native
+
+It is a type of module defined over the space of named objects whose composite objects yields an integer.
+
+integers are defined as they are usual mathematical properties but there are special class of integers whose definitions conform the mainstream theory and those stream types are called **superintegers**.
+
+$\mathbb{Z}_{0}$ and $\mathbb{Z}_{1}$ _compose_ a **supernative** $\mathcal{S}_{N}$ whose repertoire contains the set of all pairs ${\{(-1, -0), \dots, (0+, 1+)}\}$ whose transformation produces ${\{(1-, 0-), \dots, (-0,-1)}\}$ and ${\{(+1, +0), \dots, (1-, 0-)}\}$ and finally ${\{(1+, 0+), \dots, (-0, -1)}\}$.
+
+The set of all superintegers then constitute the basis $\mathbf{S}_{-0}{0-} \otimes \mathbf{S}_{-1}{1-}$
+
+
+$\mathbf{S}_{Z}{-I}$"""
+
+gist = "https://gist.github.com/superintgr/3eb8d1160d4c0ad844f952df9bcd5533"
+
+class Luminol:
+    """Constructor of the substrate where valid string of text causes error in rendering.
+
+    Gist contains two pictures of the render images of the same code string except for the last line where slight changes in the symbols were perturbed vectors.
+    The core abnormality is captured within the image variable above.
+    The IO fascilitated at the iphone safari browser viewing the github page for gist threads and switching between the content and view tabs which dynamically renders the entered text.
+
+    The goal is to construct the error inducer and its properties such that we could name its causal attributes explicitly.
+    """
+    pass
